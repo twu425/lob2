d95c2737eaf4e2c2d6fccc8d35656e24b268bfec
Tony Wu <82834362+twu425@users.noreply.github.com>
2025-05-19 22:04:01 -0700

    Partially setup gym environment, preparing for client server threading

--- server.py
+++ server.py
@@ -107,3 +107,39 @@ def make_orders(data):
 
 if __name__ == "__main__":
     socketio.run(app, port=8000, debug=True)
+
+
+
+
+
+import gymnasium as gym
+
+class battleField(gym.Env):
+    def __init__(self):
+        self.max_units = 100
+        self.unit_parameters = 9
+        self.map_size = 500
+
+        self.observation_space = gym.spaces.Box(
+            low=-np.inf,
+            high=np.inf,
+            shape=(self.max_units * self.unit_parameters,),
+            dtype=np.float32
+        )
+
+        self.action_space = gym.spaces.dict({
+            "order_type": gym.spaces.Discrete(7), 
+            "move_coordinates": gym.spaces.Box(
+                low=0.0,
+                high=self.map_size,
+                shape=(2, 16) # 2 coordinates, 16 times
+            )
+        })
+
+    def reset():
+        socketio.emit("reset")
+
+    def step(self, action):
+        
+        observation = np.zeros((self.max_units * self.unit_parameters,), dtype=np.float32)
+        
